1222461c019af681337cc63bcdd660b8a332b0a2
James Trinklein <James.Trinklein@valant.com>
2022-03-04 20:12:30 -0800

    feat: add support for update toasts

--- PrStat.Toaster/Toaster.cs
+++ PrStat.Toaster/Toaster.cs
@@ -6,12 +6,34 @@ namespace PrStat.Toaster
 {
     public class Toaster
     {
-        public static readonly string PrIdArg = "prId";
-        public static readonly string PrUrlArg = "prUrl";
-        public static readonly string ActionArg = "action";
+        public static readonly string Arg_Action = "action";
+        public static readonly string Arg_PrId = "prId";
+        public static readonly string Arg_ToastType = "toastType";
+        public static readonly string Arg_Url = "url";
         public static readonly string Action_View = "view";
         public static readonly string Action_MarkRead = "ignore";
         public static readonly string Action_Close = "close";
+        public static readonly string ToastType_Pr = "pr";
+        public static readonly string ToastType_Update = "update";
+
+        public static void GenerateToastForUpdate(string releasePageUrl, string updateVersion)
+        {
+            new ToastContentBuilder()
+                   .AddArgument(Arg_ToastType, ToastType_Update)
+                   .AddArgument(Arg_Url, releasePageUrl)
+                   .AddText($"Version {updateVersion} Now Available")
+                   .AddText($"Update now or snooze notification until next check")
+                   .AddButton(new ToastButton()
+                       .SetContent("View Release")
+                       .AddArgument(Arg_Action, Action_View)
+                       .SetBackgroundActivation())
+
+                   .AddButton(new ToastButton()
+                       .SetContent("Snooze")
+                       .AddArgument(Arg_Action, Action_Close)
+                       .SetBackgroundActivation())
+                   .Show();
+        }
 
         public static void GenerateToastForPr(PullRequest pr)
         {
@@ -19,25 +41,25 @@ namespace PrStat.Toaster
             var description = pr.Description?[..shortDescriptionLength] ?? string.Empty;
             new ToastContentBuilder()
                 .AddAttributionText(pr.Author.Name)
-                .AddArgument(PrIdArg, pr.Id)
-                .AddArgument(PrUrlArg, pr.Url)
+                .AddArgument(Arg_PrId, pr.Id)
+                .AddArgument(Arg_Url, pr.Url)
                 .AddText($"PR {pr.Id} Needs Review")
                 .AddText(pr.Title)
                 .AddText(description)
 
                 .AddButton(new ToastButton()
                     .SetContent("View")
-                    .AddArgument(ActionArg, Action_View)
+                    .AddArgument(Arg_Action, Action_View)
                     .SetBackgroundActivation())
 
                 .AddButton(new ToastButton()
                     .SetContent("Mark Read")
-                    .AddArgument(ActionArg, Action_MarkRead)
+                    .AddArgument(Arg_Action, Action_MarkRead)
                     .SetBackgroundActivation())
 
                 .AddButton(new ToastButton()
                     .SetContent("Dismiss")
-                    .AddArgument(ActionArg, Action_Close)
+                    .AddArgument(Arg_Action, Action_Close)
                     .SetBackgroundActivation())
                 .Show();
         }
