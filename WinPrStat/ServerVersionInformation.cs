e365b18d9fbb457f39567ea0f1e3adaf7473955e
James Trinklein <James.Trinklein@valant.com>
2022-03-04 20:18:45 -0800

    fix: add missing files

---
+++ WinPrStat/ServerVersionInformation.cs
@@ -0,0 +1,7 @@
+﻿namespace WinPrStat
+{
+    internal class ServerVersionInformation
+    {
+        public string Version { get; set; } = "0.0.0";
+    }
+}
---
+++ WinPrStat/UpdateInformation.cs
@@ -0,0 +1,9 @@
+﻿namespace WinPrStat
+{
+    internal class UpdateInformation
+    {
+        public string Version { get; init; } = "0.0.0";
+        public string Url { get; init; } = string.Empty;
+        public bool NewerVersionAvailable { get; init; } = false;
+    }
+}
