f42d780366da42e5b3a642facab610c85a80de05
James Trinklein <James.Trinklein@valant.com>
2022-03-04 20:15:53 -0800

    feat: fix update check utility

--- WinPrStat/UpdateUtils.cs
+++ WinPrStat/UpdateUtils.cs
@@ -1,42 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace WinPrStat
 {
-    public static class StringExtensions
+    internal static class UpdateUtils
     {
-        public static bool IsNewerVersionThan(this string mine, string testVersion)
-        {
-            var myParts = mine.Split('.').Select(x => int.Parse(x)).ToArray();
-            var testParts = testVersion.Split('.').Select(x => int.Parse(x)).ToArray();
-            return myParts[0] > testParts[0] || 
-                (myParts[0] == testParts[0] && myParts[1] > testParts[1]) ||
-                (myParts[0] == testParts[0] && myParts[1] == testParts[1] && myParts[2] > testParts[2]);
-        }
-    }
-    internal class UpdateInformation
-    {
-        public string ServerVersion { get; init; } = "7.0.0";
-        public string ZipUrl { get; init; } = string.Empty;
-        public bool NewerVersionAvailable { get; init; } = true;
-    }
-    internal class ServerVersionInformation
-    {
-        public string Version { get; set; } = "0.0.0";
-    }
-    internal class UpdateUtils
-    {
-        private readonly string serverRoot = "https://apps.trinkle.in/prstat";
-        private readonly string zipName = "winprstat";
-        private readonly string serverVersionFile = "latest.json";
+        private static readonly string serverRoot = "https://apps.trinkle.in";
+        private static readonly string serverVersionFile = "winprstat.latest.json";
 
-        private HttpClient CreateClient()
+        private static HttpClient CreateClient()
         {
             var client = new HttpClient();
             client.DefaultRequestHeaders.Accept.Clear();
@@ -46,16 +20,17 @@ namespace WinPrStat
             return client;
         }
 
-        private string GetVersionFileUrl()
+        private static string GetVersionFileUrl()
         {
             return $"{serverRoot}/{serverVersionFile}";
         }
-        private string GetZipFileUrl(ServerVersionInformation info)
+
+        private static string GetUpdatePageUrl(string version)
         {
-            return $"{serverRoot}/{zipName}_{info.Version}.zip";
+            return $"https://github.com/jtrinklein/PrStat/releases/tag/v{version}";
         }
 
-        private async Task<ServerVersionInformation> GetLatestServerVersionInformation()
+        private static async Task<ServerVersionInformation> GetLatestServerVersionInformation()
         {
             var client = CreateClient();
             var response = await client.GetAsync(GetVersionFileUrl());
@@ -67,7 +42,7 @@ namespace WinPrStat
             return await JsonSerializer.DeserializeAsync<ServerVersionInformation>(response.Content.ReadAsStream());
         }
 
-        public async Task<UpdateInformation> CheckForUpdates(string currentVersion)
+        public static async Task<UpdateInformation> CheckForUpdates(string currentVersion)
         {
             if (currentVersion.Split(".").Length < 3)
             {
@@ -82,8 +57,8 @@ namespace WinPrStat
 
             return new UpdateInformation
             {
-                ServerVersion = serverVersion.Version,
-                ZipUrl = GetZipFileUrl(serverVersion),
+                Version = serverVersion.Version,
+                Url = GetUpdatePageUrl(serverVersion.Version),
                 NewerVersionAvailable = serverVersion.Version.IsNewerVersionThan(currentVersion),
             };
         }
